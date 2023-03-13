2ebc3846d16ee917f7e9004ff3dd0f2a830709c3
Yoko Fajar Santosa <kurokot626@gmail.com>
2023-03-13 09:46:48 +0700

    new changes

--- application/views/menu2/index.php
+++ application/views/menu2/index.php
@@ -1,58 +1,55 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
+  <!-- Page Heading -->
+  <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
- 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-
-<div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card">
-      <div class="card-body">
-      <img src="<?= base_url('assets/'); ?>img/porto1.jpg" class="card-img-top " height="200" width="42" alt="...">
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <img src="<?= base_url('assets/'); ?>img/porto1.jpg" class="card-img-top " height="200" width="42" alt="...">
+        </div>
+      </div>
     </div>
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <img src="<?= base_url('assets/'); ?>img/porto2.png" class="card-img-top" height="200" width="42" alt="...">
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <div class="card-body">
-      <img src="<?= base_url('assets/'); ?>img/porto2.png" class="card-img-top" height="200" width="42" alt="...">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <img src="<?= base_url('assets/'); ?>img/porto3.png" height="200" width="42" class="card-img-top" alt="...">
+        </div>
+      </div>
     </div>
+    <div class="col"><br>
+      <div class="card">
+        <div class="card-body">
+          <img src="<?= base_url('assets/'); ?>img/porto4.jpg" class="card-img-top" height="200" width="42" alt="...">
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <div class="card-body">
-      <img src="<?= base_url('assets/'); ?>img/porto3.png" height="200" width="42" class="card-img-top" alt="...">
+    <div class="col"><br>
+      <div class="card">
+        <div class="card-body">
+          <img src="<?= base_url('assets/'); ?>img/porto5.png" class="card-img-top" height="200" width="42" alt="...">
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
-  <div class="col"><br>
-    <div class="card">
-      <div class="card-body">
-      <img src="<?= base_url('assets/'); ?>img/porto4.jpg" class="card-img-top" height="200" width="42" alt="...">
-    </div>
-    </div>
-  </div>
-  <div class="col"><br>
-    <div class="card">
-      <div class="card-body">
-      <img src="<?= base_url('assets/'); ?>img/porto5.png" class="card-img-top" height="200" width="42" alt="...">
-    </div>
-    </div>
-  </div>
-  <div class="col"><br>
-    <div class="card">
-      <div class="card-body">
-      <img src="<?= base_url('assets/'); ?>img/porto6.png" height="200" width="42" class="card-img-top" alt="...">
-    </div>
+    <div class="col"><br>
+      <div class="card">
+        <div class="card-body">
+          <img src="<?= base_url('assets/'); ?>img/porto6.png" height="200" width="42" class="card-img-top" alt="...">
+        </div>
+      </div>
     </div>
   </div>
 </div>
-                </div>
-                <!-- /.container-fluid -->
+<!-- /.container-fluid -->
 
-            </div>
-            <!-- End of Main Content -->
+</div>
+<!-- End of Main Content -->
